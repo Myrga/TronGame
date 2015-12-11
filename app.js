1603ac35681d937e18ac000efddebedbb01341fa
nitneuq86 <nitneuq86@gmail.com>
2015-12-11 09:33:30 +0100

    Modification point de départ murs dans dernier point du dernier mur

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var serveur = require('tron-serveur');
 serveur.setArenaSize(1000,800);
 serveur.setFPS(60);
 serveur.setPlayerSpeed(2);
-serveur.setPlayerInvincibilityTime(6);
+serveur.setPlayerInvincibilityTime(3);
 serveur.run(3000);
